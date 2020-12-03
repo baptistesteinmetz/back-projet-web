953c71d76d038cbeb25c063270fb9040fc0fb1cb
baptiste.steinmetz <baptiste.steinmetz67@gmail.com>
2020-12-03 13:29:29 +0100

    test

--- public/index.php
+++ public/index.php
@@ -1,13 +1,16 @@
 <?php
 
-use Slim\Factory\AppFactory;
+// use Slim\Factory\AppFactory;
 
-require __DIR__ . '/../vendor/autoload.php';
+// require __DIR__ . '/../vendor/autoload.php';
 
-$app = AppFactory::create();
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/..");
-$dotenv->load();
-$routes = require __DIR__ . '/../router.php'; 
-$routes($app);
+// $app = AppFactory::create();
+// $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/..");
+// $dotenv->load();
+// $routes = require __DIR__ . '/../router.php'; 
+// $routes($app);
 
-$app->run();
+// $app->run();
+
+echo "hello world";
+
