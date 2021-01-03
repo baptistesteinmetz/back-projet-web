a527922d09e6045b1c4e1c27e6e6f6ee3b5f7b87
baptiste.steinmetz <baptiste.steinmetz67@gmail.com>
2021-01-03 14:49:00 +0100

    test

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -178,7 +178,7 @@ class UserController {
     public function getUser(Request $request, Response $response, array $args) {
         require_once  __DIR__ . './../../bootstrap.php';
         $id = intval($args['id']);
-        $user = $entityManager->getRepository('User');
+        $user = $entityManager->getRepository('User')->findAll();
         var_dump($user);        
         // if($user) {
         //     $data = [
