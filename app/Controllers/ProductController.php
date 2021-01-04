438d401549e3b53b5b135af1623b49866faecc13
baptistesteinmetz <baptiste.steinmetz67@gmail.com>
2021-01-04 18:45:01 +0100

    post

--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -81,10 +81,9 @@ class ProductController {
 
     public function buyArticle(Request $request, Response $response, $args) {
         require_once __DIR__ . './../../bootstrap.php';
-        var_dump($args);
+        var_dump($request->getParsedBody());
         $products = $args['data'];
         var_dump($products);
-
         $price = 0;
         foreach($products as $product) {
             /** @var Product $product  */
